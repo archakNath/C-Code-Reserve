d3f461ada22afc3cf25a6513208dc14035132335
Archak Nath <112867859+archakNath@users.noreply.github.com>
2022-09-30 19:35:33 +0530

    Create print0to10.c

---
+++ CFiles/print0to10.c
@@ -0,0 +1,16 @@
+/*
+    WAP to print 0 to 10
+*/
+
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("Series: \n");
+    for (int i = 0; i <= 10; i++)
+    {
+        printf("%d ", i);
+    }
+    
+    return 0;
+}
