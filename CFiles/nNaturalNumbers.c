e05656c59981d992207dd50b6b82b2c66e05fca2
Archak Nath <112867859+archakNath@users.noreply.github.com>
2022-09-30 20:03:45 +0530

    print sum of n natural numbers

--- CFiles/nNaturalNumbers.c
+++ CFiles/nNaturalNumbers.c
@@ -3,6 +3,7 @@
     input value of n
     print all numbers from 1 to n
     then print all number from n to 1(reverse)
+    also print sum of n natural numbers
 */
 
 #include<stdio.h>
@@ -12,6 +13,7 @@ int main(int argc, char const *argv[])
     int n, c = 1;
     printf("Enter the value of n: ");
     scanf("%d", &n);
+    int sum = 0;
 
     //print 1 to n
     printf("All natural numbers till n. \n");
@@ -27,9 +29,12 @@ int main(int argc, char const *argv[])
     while (n>=1)
     {
         printf("%d ", n);
+        sum+=n;
         n--;
     }
     
+    //sum of n natural numbers
+    printf("The sum of natural numbers is %d.", sum);
     
     return 0;
 }
