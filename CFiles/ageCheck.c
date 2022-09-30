ace037c805c3708afdd2dcf92c1778a86ef0de5b
Archak Nath <112867859+archakNath@users.noreply.github.com>
2022-09-30 11:45:37 +0530

    added ternary operator for the same problem

--- CFiles/ageCheck.c
+++ CFiles/ageCheck.c
@@ -1,3 +1,8 @@
+/*
+    simple program to check age
+    input the age of the user
+    print appropriate message
+*/
 #include<stdio.h>
 
 int main(int argc, char const *argv[])
@@ -6,6 +11,7 @@ int main(int argc, char const *argv[])
     printf("Enter your age: ");
     scanf("%d", &age);
 
+    //condition checking using if elseif else
     if(age>=18){
         printf("You are an adult.");
     } else if((age>=13)&&(age<18)){
@@ -13,5 +19,9 @@ int main(int argc, char const *argv[])
     } else{
         printf("You are a child.");
     }
+
+    //ternery operator
+    (age>=18)?printf("you are an adult"):printf("you are not an adult");
+
     return 0;
 }
