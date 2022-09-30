522ae1ec357b6b0259fa1726393d8acbfd12b469
Archak Nath <112867859+archakNath@users.noreply.github.com>
2022-09-30 20:41:08 +0530

    Create starPattern.c

---
+++ CFiles/starPattern.c
@@ -0,0 +1,24 @@
+/*
+    Q. Print the following pattern using nested loop.
+    *****
+    *****
+    *****
+    *****
+*/
+
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+        
+    }
+    
+    return 0;
+}
