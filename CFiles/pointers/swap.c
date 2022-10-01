82caa3dd8af453f1016a4ac48aa7e5813b1ead53
Archak Nath <112867859+archakNath@users.noreply.github.com>
2022-10-01 21:18:51 +0530

    Create swap.c

---
+++ CFiles/pointers/swap.c
@@ -0,0 +1,25 @@
+/*
+    Q. Write a program to swap the value of two variables
+*/
+
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int a = 25, b = 38;
+    printf("The value of a is %d \n", a);
+    printf("The value of b is %d \n", b);
+
+    int *aptr = &a;
+    int *bptr = &b;
+
+    //swaping happens here
+    int temp = *aptr;
+    *aptr = *bptr;
+    *bptr = temp;
+
+    printf("\nThe value of a is %d \n", a);
+    printf("The value of b is %d \n", b);
+
+    return 0;
+}
